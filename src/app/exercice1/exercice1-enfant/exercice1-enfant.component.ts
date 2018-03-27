622d67698cfd653c9ad08de7926bea3fe1dfe8a8
scalpatt <emploi@patte.fr>
2018-03-27 17:12:50 +0200

    projet3

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 
 @Component({
   selector: 'app-exercice1-enfant',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Exercice1EnfantComponent implements OnInit {
 
+  @Output() public eventUpdateCptr: EventEmitter<number> = new EventEmitter();
+  @Input() public compteur: number;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  public updateCompteur(value: number): void {
+    this.eventUpdateCptr.emit(value);
+  }
 }
--- src/app/exercice2/exercice2.component.ts
+++ src/app/exercice2/exercice2.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-exercice2',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercice2.component.css']
 })
 export class Exercice2Component implements OnInit {
+
+  @ViewChild('textInput')   public el: ElementRef;
+
   public valeur: string;
 
   constructor() { }
@@ -13,4 +16,7 @@ export class Exercice2Component implements OnInit {
   ngOnInit() {
   }
 
+  public textUpdate(): void {
+     this.valeur = this.el.nativeElement.value;
+  }
 }
